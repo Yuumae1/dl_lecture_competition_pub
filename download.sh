c0bfc21f3a564dd2525c76d5d0f1382cb8102769
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-07-16 12:41:23 +0900

    add main_v2

---
+++ download.sh
@@ -0,0 +1,6 @@
+FILE_ID="1VRc_ERfP2OUY_t7ppkMRxn-ZFHc_6tux";
+FILE_NAME="train.json";
+
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
+rm -f /tmp/cookies.txt
